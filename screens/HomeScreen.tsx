6ce969a07336301a85567fe170f97cca36f7abde
vong3432 <vongnyuksoon2000@gmail.com>
2026-01-09 22:23:20 +0800

    feat: add logout

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import { ScrollView, Text } from 'react-native';
+import { useAuthContext } from '../libs/auth/useAuthContext';
+import { useAppTheme } from '../ui/theme';
+import { Button, ButtonVariant } from '../ui/components/Button';
 
 export default function HomeScreen() {
+  const { auth, dispatch } = useAuthContext();
+  const theme = useAppTheme();
+
+  const handleLogout = () => {
+    dispatch({ type: 'logout' });
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.backgroundColor,
+        padding: theme.spacing.md,
+      }}
+    >
+      <Text>Username: {auth.authInfo?.name ?? ''}</Text>
+      <Text>Email: {auth.authInfo?.email ?? ''}</Text>
+      <Button
+        variant={ButtonVariant.bordered}
+        color={theme.colors.text}
+        title="Logout"
+        style={{ marginTop: theme.spacing.xxl }}
+        onPress={handleLogout}
+      ></Button>
+    </ScrollView>
   );
 }
